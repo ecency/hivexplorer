c9708fcb85f015b498d26aa6188d93bef558a391
feruzm <fmiboy@gmail.com>
2022-12-06 15:59:56 +0530

    cleaner solution

--- src/server/util.ts
+++ src/server/util.ts
@@ -31,10 +31,8 @@ export const cleanURL = (req: any, res: any, next: any) => {
   if (req.url.includes('@@')) {
     res.redirect(req.url.replace(new RegExp('@{2,}', 'g'), '@'));
   }
-  if (req.url.includes('-hs?code')) {
-    next();
-  } else if (!req.url.includes('/api/')) {
-    if (req.url !== req.url.toLowerCase()) {
+  if (req.url !== req.url.toLowerCase()) {
+    if (!req.url.includes('/api/')) {
       res.redirect(301, req.url.toLowerCase());
     } else {
       next();
